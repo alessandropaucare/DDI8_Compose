07966446dc8c008863ccbf9bc13bdc9fb95fc5d0
ALESPAU <apaucare27@educa.jcyl.es>
2025-01-24 20:30:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ddi8_tarea1_ape/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ddi8_tarea1_ape
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ddi8_tarea1_ape.ui.theme.DDI8_Tarea1_APETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DDI8_Tarea1_APETheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DDI8_Tarea1_APETheme {
+        Greeting("Android")
+    }
+}
