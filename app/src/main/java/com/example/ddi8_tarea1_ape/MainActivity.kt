9e18fb986ab096ee863833f2b1078c97fc282b8e
ALESPAU <apaucare27@educa.jcyl.es>
2025-02-05 19:47:47 +0100

    small fix

--- app/src/main/java/com/example/ddi8_tarea1_ape/MainActivity.kt
+++ app/src/main/java/com/example/ddi8_tarea1_ape/MainActivity.kt
@@ -1,42 +1,42 @@
 package com.example.ddi8_tarea1_ape
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.ddi8_tarea1_ape.ui.theme.DDI8_Tarea1_APETheme
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             DDI8_Tarea1_APETheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    SmallTopAppBarExample()
-                }
+                    MainContent()
             }
         }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        // Search Bar
+        SmallTopAppBarExample()
+
     }
 }
 
@@ -61,3 +61,5 @@ fun SmallTopAppBarExample() {
         }
        )
 }
+
+
